01ef32e16d9cbd0fbb938012d3d58c14676f6cee
Hamza Çiçek <126699123+Hmzcck@users.noreply.github.com>
2024-08-02 20:22:33 +0300

    Add TaskDto and TaskEntity classes

---
+++ todoapplication/src/main/java/com/hamzacicek/todoapplication/business/dto/TaskDto.java
@@ -0,0 +1,46 @@
+package com.hamzacicek.todoapplication.business.dto;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+// LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+public class TaskDto implements Serializable {
+    // id, title, description, status, due date, created at
+
+    public static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @NotEmpty(message = "{task.title.validation.constraints.NotEmpty.message}")
+    @Size(min = 1, max = 255, message = "{task.title.validation.constraints.Size.message}")
+    private String title;
+
+    @Size(max = 1000, message = "{task.description.validation.constraints.Size.message}")
+    private String description;
+
+    @Builder.Default
+    private Long status = 0L;
+
+    @NotNull(message = "{task.dueDate.validation.constraints.NotNull.message}")
+    @FutureOrPresent(message = "{task.dueDate.validation.constraints.FutureOrPresent.message}")
+    private LocalDate dueDate;
+
+    @Builder.Default
+    private Date createdAt = new Date(System.currentTimeMillis());
+}
---
+++ todoapplication/src/main/java/com/hamzacicek/todoapplication/data/entity/TaskEntity.java
@@ -0,0 +1,47 @@
+package com.hamzacicek.todoapplication.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+// LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+@Entity
+@Table(name = "task")
+public class TaskEntity implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Long status = 0L;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Builder.Default
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date(System.currentTimeMillis());
+}
